a455525e39a1984846c6142398e8a4e0b4771e8a
andrey kugubaev <egor.tolbaev@esplus.ru>
2023-03-21 16:52:58 +0500

    Initial commit

---
+++ src/main/java/com/example/test_secuirity2db_thymleaf/TestSecuirity2dbThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_secuirity2db_thymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSecuirity2dbThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSecuirity2dbThymleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_secuirity2db_thymleaf/TestSecuirity2dbThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_secuirity2db_thymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSecuirity2dbThymleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
